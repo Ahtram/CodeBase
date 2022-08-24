fd17fe7d4ea4b6ec20a5042a33ca64d913ada2be
Neo <uninet.neo@gmail.com>
2022-08-24 18:38:28 +0800

    Testing UnityCloud build to change the buildNumber of the CloudBuildMenifest.

--- Editor/Other/CloudBuildHelper.cs
+++ Editor/Other/CloudBuildHelper.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using Teamuni.Codebase;
 
 public static class CloudBuildHelper {
 #if UNITY_CLOUD_BUILD && (UNITY_STANDALONE || UNITY_IOS || UNITY_ANDROID)
@@ -9,16 +10,20 @@ public static class CloudBuildHelper {
     /// <param name="manifest"></param>
     public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
     {
-        string build = "";
-        if (manifest.TryGetValue("buildNumber", out build)) {
-            PlayerSettings.bundleVersion = string.Format("{0}.{1}", PlayerSettings.bundleVersion, build);
-            PlayerSettings.iOS.buildNumber = build;
-        
-            int buildNo = 1;
-            int.TryParse(build, out buildNo);
-        
-            PlayerSettings.Android.bundleVersionCode = buildNo;
-        }
+        //[Dep]: I don't like this.
+        // string build = "";
+        // if (manifest.TryGetValue("buildNumber", out build)) {
+        //     PlayerSettings.bundleVersion = string.Format("{0}.{1}", PlayerSettings.bundleVersion, build);
+        // }
+
+        //Test: Setup the manifest buildNumber?? Will this work?!
+        manifest.SetValue("buildNumber", Version.BundleVersion());
+
+        //[iOS stuff]: a string buildNumber
+        // PlayerSettings.iOS.buildNumber = build;
+    
+        //[Android stuff]: An int less than 100000.
+        // PlayerSettings.Android.bundleVersionCode = buildNo;
     }
 #endif
 }
