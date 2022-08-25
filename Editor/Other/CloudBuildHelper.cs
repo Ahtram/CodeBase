87c072098b49d5540dff69f662ca1a3a2c84c4ce
Neo <uninet.neo@gmail.com>
2022-08-25 14:28:05 +0800

    Testing the new bundleVersion support for display cloud build target.

--- Editor/Other/CloudBuildHelper.cs
+++ Editor/Other/CloudBuildHelper.cs
@@ -1,29 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEditor;
-using Teamuni.Codebase;
 
 public static class CloudBuildHelper {
+
 #if UNITY_CLOUD_BUILD && (UNITY_STANDALONE || UNITY_IOS || UNITY_ANDROID)
     /// <summary>
     /// This method will be execute by UnityCloud before a build happen.
     /// </summary>
     /// <param name="manifest"></param>
-    public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
-    {
-        //[Dep]: I don't like this.
-        // string build = "";
-        // if (manifest.TryGetValue("buildNumber", out build)) {
-        //     PlayerSettings.bundleVersion = string.Format("{0}.{1}", PlayerSettings.bundleVersion, build);
-        // }
-
-        //Test: Setup the manifest buildNumber?? Will this work?!
-        manifest.SetValue("buildNumber", Version.BundleVersion());
-
-        //[iOS stuff]: a string buildNumber
-        // PlayerSettings.iOS.buildNumber = build;
-    
-        //[Android stuff]: An int less than 100000.
-        // PlayerSettings.Android.bundleVersionCode = buildNo;
+    public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest) {
+        string build = "";
+        string cloudBuildTargetName = "";
+        manifest.TryGetValue("cloudBuildTargetName", out cloudBuildTargetName);
+        if (manifest.TryGetValue("buildNumber", out build)) {
+            //Here we custom to bundleVersion for cloud build.
+            PlayerSettings.bundleVersion = string.Format("{0} #{1} {2}", PlayerSettings.bundleVersion, build, cloudBuildTargetName);
+            PlayerSettings.iOS.buildNumber = build;
+        
+            int buildNo = 1;
+            int.TryParse(build, out buildNo);
+        
+            PlayerSettings.Android.bundleVersionCode = buildNo;
+        }
     }
 #endif
+
 }
