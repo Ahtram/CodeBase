53022b8f0fe23d803c399fd3c29029d2f8cb5136
Neo <uninet.neo@gmail.com>
2022-09-17 20:42:49 +0800

    Remove the cloud build helper for good.

--- Editor/Other/CloudBuildHelper.cs
+++ Editor/Other/CloudBuildHelper.cs
@@ -1,28 +1,29 @@
-using UnityEngine;
-using UnityEditor;
+//[Dep]: I guess we should not put these things in a plugin.
+// using UnityEngine;
+// using UnityEditor;
 
-public static class CloudBuildHelper {
+// public static class CloudBuildHelper {
 
-#if UNITY_CLOUD_BUILD && (UNITY_STANDALONE || UNITY_IOS || UNITY_ANDROID)
-    /// <summary>
-    /// This method will be execute by UnityCloud before a build happen.
-    /// </summary>
-    /// <param name="manifest"></param>
-    public static void SetupBundleVersionFromCloudManifest(UnityEngine.CloudBuild.BuildManifestObject manifest) {
-        string build = "";
-        string cloudBuildTargetName = "";
-        manifest.TryGetValue("cloudBuildTargetName", out cloudBuildTargetName);
-        if (manifest.TryGetValue("buildNumber", out build)) {
-            //Here we custom to bundleVersion for cloud build.
-            PlayerSettings.bundleVersion = string.Format("{0} #{1} {2}", PlayerSettings.bundleVersion, build, cloudBuildTargetName);
-            PlayerSettings.iOS.buildNumber = build;
-        
-            int buildNo = 1;
-            int.TryParse(build, out buildNo);
-        
-            PlayerSettings.Android.bundleVersionCode = buildNo;
-        }
-    }
-#endif
+// #if UNITY_CLOUD_BUILD && (UNITY_STANDALONE || UNITY_IOS || UNITY_ANDROID)
+//     /// <summary>
+//     /// This method will be execute by UnityCloud before a build happen.
+//     /// </summary>
+//     /// <param name="manifest"></param>
+//     public static void SetupBundleVersionFromCloudManifest(UnityEngine.CloudBuild.BuildManifestObject manifest) {
+//         string build = "";
+//         string cloudBuildTargetName = "";
+//         manifest.TryGetValue("cloudBuildTargetName", out cloudBuildTargetName);
+//         if (manifest.TryGetValue("buildNumber", out build)) {
+//             //Here we custom to bundleVersion for cloud build.
+//             PlayerSettings.bundleVersion = string.Format("{0} #{1} {2}", PlayerSettings.bundleVersion, build, cloudBuildTargetName);
+//             PlayerSettings.iOS.buildNumber = build;
 
-}
+//             int buildNo = 1;
+//             int.TryParse(build, out buildNo);
+
+//             PlayerSettings.Android.bundleVersionCode = buildNo;
+//         }
+//     }
+// #endif
+
+// }
